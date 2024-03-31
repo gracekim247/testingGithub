e15e9205a494f0712bd233a829aec8deea1420c4
gracekim24 <gracekimm247@gmail.com>
2024-03-30 22:36:20 -0400

    java addition

--- testing.java
+++ testing.java
@@ -1,1 +1,9 @@
-System.out.println("Hello World"); 
+class testing{
+   public static void main(String[] args) 
+   {
+      System.out.println("Hello World"); 
+      for (int i = 0; i<9; i++){
+         System.out.print(i);
+      }
+   }
+}
